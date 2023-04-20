18e641b2afa5664d1a9264a1d97cc8a8a2924c8d
nishang123 <72482858+nishang123@users.noreply.github.com>
2023-04-20 14:37:45 +0800

    添加管理用户界面，单纯的在views加入了我的文件

---
+++ front_end/src/views/UserManage.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/" style="padding-left: 10px;padding-bottom: 10px;font-size: 12px">
+      <el-breadcrumb-item :to="{ path: '/main' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 用户列表卡片 -->
+    <div>
+      <el-input v-model="searchId" placeholder="Search by ID" width:70px></el-input>
+      <el-button type="primary" @click="search">Search</el-button>
+      <el-table :data="users" style="width: 100%">
+        <el-table-column prop="id" label="ID"></el-table-column>
+        <el-table-column prop="username" label="Name"></el-table-column>
+        <el-table-column prop="phoneNumber" label="PhoneNumber"></el-table-column>
+        <el-table-column prop="description" label="Description"></el-table-column>
+        <el-table-column label="Action">
+          <template v-slot="scope">
+            <el-button type="danger" @click="deleteUser(scope.row.id)">Delete</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import QueryString from 'qs';
+export default {
+  name: 'UserManage',
+  data() {
+    return {
+      users: [{ id: 1, username: '张三', phoneNumber: 123456, description: '测试样例' }],
+      searchId: ''
+    };
+  },
+  created() {
+    this.getUsers();
+  },
+  methods: {
+    //获取所有用户，不传入参数
+    getUsers() {
+      axios.get('/user/displayAllUser')
+        .then(response => {
+          console.log(response.data);
+          this.users = [];
+          for (var user in response.data.users) {
+            this.users.push(response.data.users[user]);
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    //删除某一用户
+    deleteUser(id) {
+      console.log(id);
+      axios.post('/user/delete', QueryString.stringify(id))
+        .then(response => {
+          console.log(response.data);
+          this.getUsers();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    search() {
+      if (this.searchId !== '') {
+        axios.get(`/user/searchbyid/${this.searchId}`)
+          .then(response => {
+            console.log(response.data);
+            this.users = [];
+            for (var user in response.data.users) {
+              this.users.push(response.data.users[user]);
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      } else {
+        this.getUsers();
+      }
+    }
+  }
+};
+</script>
